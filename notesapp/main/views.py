2d88087fe8547485e251d0add85bd163cbf546a4
ColinSmith14 <106163641+ColinSmith14@users.noreply.github.com>
2022-11-17 14:16:01 -0500

    work progress Day 1

--- notesapp/main/views.py
+++ notesapp/main/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+class note():
+    def __init__(self, content):
+        self.content = content
+
+def index(request):
+    if "notes" not in request.session:
+        request.session["notes"] = []
+    return render(request, "main/index.html",{
+        "notes": request.session["notes"]
+    })
+        
